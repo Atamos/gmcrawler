440e7464951921cb70a5066517c6e6a607ffa4ad
Marco Sciatta <marco.sciatta@gmail.com>
2014-05-06 22:18:14 +0200

    added cli parameters for size and paging

--- gommadiretto/spiders/gm.py
+++ gommadiretto/spiders/gm.py
@@ -14,19 +14,30 @@ from scrapy.contrib.loader import ItemLoader
 class GmSpider(CrawlSpider):
 	name = "gm"
 	domain_name = "gommadiretto.it"
+	
 	#start_urls = ["http://www.gommadiretto.it/cgi-bin/rshop.pl?dsco=130&cart_id=9963841.130.4327&sowigan=So&Breite=205&Quer=55&Felge=16&Speed=&kategorie=6&Marke=&ranzahl=4&tyre_for=&x_tyre_for=&suchen=Trova%20pneumatici&rsmFahrzeugart=PKW&search_tool=standard&Ang_pro_Seite=5"]
 	#start_urls = ["http://www.gommadiretto.it/cgi-bin/rshop.pl?m_s=3&Ang_pro_Seite=5&rsmFahrzeugart=PKW&s_p=Tutti&cart_id=9963841.130.4327&x_tyre_for=&search_tool=standard&dsco=130&tyre_for=&suchen=Trova+pneumatici&with_bootstrap_flag=1&ranzahl=4&sowigan=&Breite=145&Quer=70&Felge=12&Speed=&Load=&Marke=&kategorie=6&filter_preis_von=&filter_preis_bis=&homologation="]
-	start_urls = ["http://www.gommadiretto.it/cgi-bin/rshop.pl?dsco=130&cart_id=9963841.130.4327&Breite=145&Quer=70&Felge=12&Speed=&Load=&kategorie=6&Marke=&ranzahl=4&tyre_for=&x_tyre_for=&m_s=3&Ang_pro_Seite=5&rsmFahrzeugart=PKW&filter_preis_bis=&filter_preis_von=&homologation=&search_tool=standard&Label=G-E-70-2&details=Ordern&typ=R-123525"]
+	#start_urls = ["http://www.gommadiretto.it/cgi-bin/rshop.pl?dsco=130&cart_id=9963841.130.4327&Breite=145&Quer=70&Felge=12&Speed=&Load=&kategorie=6&Marke=&ranzahl=4&tyre_for=&x_tyre_for=&m_s=3&Ang_pro_Seite=5&rsmFahrzeugart=PKW&filter_preis_bis=&filter_preis_von=&homologation=&search_tool=standard&Label=G-E-70-2&details=Ordern&typ=R-123525"]
 
 	rules = (
 		Rule(SgmlLinkExtractor(allow=('cgi-bin\/rshop.pl\?.*typ=[-0-9a-zA-Z]*$')),callback='parse_item'),
 		#Rule(SgmlLinkExtractor(restrict_xpaths=('//a[@id="ajax_suchergebnisliste_goto_next"]',)))
 	)
 
-	def __init__(self,name=None,**kwargs):
+	def __init__(self,name=None,dim1=None,dim2=None,dim3=None,paging=5,**kwargs):
 		ScrapyFileLogObserver(open("spider.log",'w'), level=log.INFO).start()
 		ScrapyFileLogObserver(open("spider_error.log",'w'),level=log.ERROR).start()
+		log.msg('begin arguments')
+		log.msg(dim1)
+		log.msg(dim2)
+		log.msg(dim3)
+		log.msg(paging)
+		log.msg('end arguments')
+		log.msg('STAR WITH : %s %s %s WITH PAGE %s' % (dim1,dim2,dim3,paging))
 		super(GmSpider,self).__init__(name,**kwargs)
+		log.msg("http://www.gommadiretto.it/cgi-bin/rshop.pl?dsco=130&Breite=%s&Quer=%s&Felge=%s&Speed=&Load=&kategorie=&Marke=&ranzahl=4&tyre_for=&x_tyre_for=&m_s=3&suchen=Trova%%20pneumatici&rsmFahrzeugart=PKW&filter_preis_bis=&filter_preis_von=&homologation=&search_tool=standard&Ang_pro_Seite=%s" % (dim1,dim2,dim3,paging))
+		self.start_urls = ["http://www.gommadiretto.it/cgi-bin/rshop.pl?dsco=130&Breite=%s&Quer=%s&Felge=%s&Speed=&Load=&kategorie=&Marke=&ranzahl=4&tyre_for=&x_tyre_for=&m_s=3&suchen=Trova%%20pneumatici&rsmFahrzeugart=PKW&filter_preis_bis=&filter_preis_von=&homologation=&search_tool=standard&Ang_pro_Seite=%s" % (dim1,dim2,dim3,paging)] 
+
 
 
 	def parse_item(self,response):
@@ -57,7 +68,6 @@ class GmSpider(CrawlSpider):
 		l.add_xpath('name',".//*[@id='detail-page']/@data-item_id")
 
 		link = sel.xpath(".//*[@id='detail-page']/div[1]/div/div/img/@src").extract()
-		log.msg(link)
 
 		l.add_value("image_urls",link)
 
