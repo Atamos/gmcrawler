297eb55cda26434fa13e36f01b7ec84c25f7dcdd
Marco Sciatta <marco.sciatta@gmail.com>
2014-05-08 00:08:26 +0200

    fix first page problem with parse_start_url function

--- gommadiretto/spiders/tinygm.py
+++ gommadiretto/spiders/tinygm.py
@@ -16,10 +16,10 @@ class TinyGmSpider(CrawlSpider):
     domain_name = "gommadiretto.it"
     
     rules = (
-         Rule(SgmlLinkExtractor(restrict_xpaths=('//a[@id="ajax_suchergebnisliste_goto_next"]')),callback="parse_items"),
+         Rule(SgmlLinkExtractor(restrict_xpaths=('//a[@id="ajax_suchergebnisliste_goto_next"]'),canonicalize=False),callback="parse_items"),
     )
 
-    def __init__(self,name=None,dim1=None,dim2=None,dim3=None,paging=50,**kwargs):
+    def __init__(self,name=None,dim1=None,dim2=None,dim3=None,paging=5,**kwargs):
         ScrapyFileLogObserver(open("tinyspider.log",'w'), level=log.INFO).start()
         ScrapyFileLogObserver(open("tinyspider_error.log",'w'),level=log.ERROR).start()
         log.msg('begin arguments')
@@ -34,18 +34,20 @@ class TinyGmSpider(CrawlSpider):
         self.start_urls = ["http://www.gommadiretto.it/cgi-bin/rshop.pl?dsco=130&Breite=%s&Quer=%s&Felge=%s&Speed=&Load=&kategorie=&Marke=&ranzahl=4&tyre_for=&x_tyre_for=&m_s=3&suchen=Trova%%20pneumatici&rsmFahrzeugart=PKW&filter_preis_bis=&filter_preis_von=&homologation=&search_tool=standard&Ang_pro_Seite=%s" % (dim1,dim2,dim3,paging)] 
 
 
+    def parse_start_url(self, response):
+        return self.parse_items(response)
+
     def parse_items(self,response):
         response = response.replace(body=response.body.replace('<br />', '\n')) 
         sel = HtmlXPathSelector(response)
         
         products = []
-        l = ItemLoader(item=GommadirettoItem(),response=response)       
-
         items = sel.xpath("//div[@id='ajax_suchergebnisliste']/*[div]")
 
         for product in items:
-            log.msg('parsing')
+            l = ItemLoader(item=DisponibilityItem(),selector=product)
             l.add_xpath('name','.//a[1]/@name')
+            l.add_xpath('title','.//div/div/div[2]/div//text()')
             l.add_xpath('disponibility','.//div/div/div[5]/div[1]/text()')
             products.append(l.load_item());
         return products
